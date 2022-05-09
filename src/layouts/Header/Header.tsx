b81bb54b9eefe95a4aefc17a9420cdd28457bbdb
Olha_Diachek <olha.diachek@litslink.com>
2022-05-09 21:57:04 +0300

    implement header

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+
 const Header = () => {
-  return <header>GitHub App</header>;
+  return (
+    <header className={styles.root}>
+      <Link to="/" className={styles.logo}>
+        <img className={styles.logoImage} alt="logo" src="logo.png" />
+      </Link>
+      <h1 className={styles.title}>GitHub</h1>
+    </header>
+  );
 };
 
 export default Header;
